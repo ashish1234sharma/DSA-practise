6357fd8a9552d5a669613f9a4e5736ae25a4da8b
ashish1234sharma <96169882+ashish1234sharma@users.noreply.github.com>
2022-05-14 22:26:38 +0530

    update

--- startPattern.js
+++ startPattern.js
@@ -2,11 +2,36 @@
 
 var n=5
 
-for(var i=1; i<=n; i++){
-    var bag=""
-    for(var j=n; j>=i; j--){
-        bag=bag+" "+"*"
+// for(var i=1; i<=n; i++){
+//     var bag=""
+//     for(var j=n; j>=i; j--){
+//         bag=bag+" "+"*"
         
+//     }
+//     console.log(bag)
+// 
+
+function fun(n){
+    for(var i=0;i<n;i++){
+        let bag=""
+        for(j=0;j<n;j++){
+            if(j==n-1){
+                 if(j<n-i){
+                bag=bag+"*"
+            }
+            else{
+                bag=bag+" "
+            }
+            }
+            else if(j<n-i){
+                bag=bag+"*"+" "
+            }
+            else{
+                bag=bag+" "+" "
+            }
+            
+            
+        }
+        console.log(bag)
     }
-    console.log(bag)
-}
+  }
