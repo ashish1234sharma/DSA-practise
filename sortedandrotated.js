339b0067bf64eef4aba77b23527b8ca10bae4fc7
ashish1234sharma <96169882+ashish1234sharma@users.noreply.github.com>
2022-05-25 15:52:50 +0530

    update

--- sortedandrotated.js
+++ sortedandrotated.js
@@ -1,5 +1,5 @@
 
-var arr =[3,4,5,6,7,1,2]
+var arr =[3 ,6 ,7 ,8 ,9 ,10 ,11 ,13 ,14 ,15 ,16 ,17 ,20 ,22 ,23 ,24 ,25 ,26 ,27 ,28, -9, -8, -7, -6, -5, -4, -3, -1, 1, 2]
 
 var n = arr.length
 
@@ -11,8 +11,21 @@ function sorted(arr,n){
     var end=n-1
     while(start<=end){
         var mid=Math.floor(start+((end-start)/2))
-        
-    }
 
-}
+        if(arr[mid]>arr[mid-1] && arr[mid] >arr[mid+1]){
+            return true
+        }
+
+        if(arr[mid] < arr[mid-1]){
+            end=mid-1 ;
+        }else if(arr[mid]<arr[mid+1]){
+            start=mid+1
+        }
+        
+        }
+        return false  
+    }
+   
+
+
 console.log(sorted(arr,n))
